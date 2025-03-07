438e16fa192c1710f1aa8ee23d9d2c9cea67af36
zxcpisya <mamin.papa0@gmail.com>
2025-03-07 12:23:12 +1000

    optimize imports, start main task

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from scripts.models import *
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/api/comments/<username>")
+@app.route("/api/comments/<username>", methods=['GET'])
 def comments_of_user():
+
     return 123
 
-@app.route("/api/general/<username>")
-def general():
-    return 123
+@app.route("/api/general/<username>", methods=['GET'])
+def general_info_about_user():
+    username = request.args.get("username")
+
+    user = User.get(User.login == username)
+
+    request_ = Log.select().where(Log.user_id == user.id)
+
+    print([i for i in request_])
+    return 'smth'
