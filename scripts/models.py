7e09fe3c854e6b9cb3b505d0f8219fc7c64cc97d
zxcpisya <mamin.papa0@gmail.com>
2025-03-07 10:28:07 +1000

    alter log models, create Author models

--- scripts/models.py
+++ scripts/models.py
@@ -1,31 +1,50 @@
 from databases import *
 
 # LOGS DATABASE CLASSES
-class Space_type(Model):
-	id = PrimaryKeyField()
-	name = CharField()
-
+class BaseLogModel(Model):
 	class Meta:
 		database = logs_db
 
-
-class Event_type(Model):
+class SpaceType(BaseLogModel):
 	id = PrimaryKeyField()
 	name = CharField()
 
-	class Meta:
-		database = logs_db
+
+class EventType(BaseLogModel):
+	id = PrimaryKeyField()
+	name = CharField()
 
 
-class Logs(Model):
+class Log(BaseLogModel):
 	id = PrimaryKeyField()
 	datetime = DateField()
 	user_id = IntegerField()
-	space_type_id = ForeignKeyField(Space_type.id)
-	event_type_id = ForeignKeyField(Event_type.id)
+	space_type_id = ForeignKeyField(SpaceType.id)
+	event_type_id = ForeignKeyField(EventType.id)
 
-	class Meta:
-		database = logs_db
 
 # AUTHORS DATABASE CLASSES
-pass
+class BaseAuthorModel(Model):
+	class Meta:
+		database = authors_db
+
+
+class User(BaseAuthorModel):
+	id = PrimaryKeyField()
+	email = CharField(unique=True)
+	login = CharField(unique=True)
+
+
+class Blog(BaseAuthorModel):
+	id = PrimaryKeyField()
+	owner_id = ForeignKeyField(User.id)
+	name = CharField()
+	description = CharField()
+
+
+class Post(BaseAuthorModel):
+	id = PrimaryKeyField()
+	author_id = ForeignKeyField(User.id)
+	header = TextField()
+	text = TextField()
+	blog_id = ForeignKeyField(Blog.id)
