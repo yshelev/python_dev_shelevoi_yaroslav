1ebfcae9cebca509e5b99a86489e4bb5d30c7f5b
zxcpisya <mamin.papa0@gmail.com>
2025-03-07 12:23:42 +1000

    optimize imports, create functions for create and discard connections

--- scripts/databases.py
+++ scripts/databases.py
@@ -1,13 +1,22 @@
-from peewee import *
+from peewee import SqliteDatabase
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-DATABASES = 'databases/'
+DATABASES = '../databases/'
 
 logs = os.getenv("logs_database")
 authors = os.getenv("authors_database")
 
-logs_db = SqliteDatabase(DATABASES + logs)
-authors_db = SqliteDatabase(DATABASES + authors)
+logs_db = SqliteDatabase(DATABASES + logs, pragmas= {
+	'foreign_keys': 1
+})
+authors_db = SqliteDatabase(DATABASES + authors, pragmas= {
+	'foreign_keys': 1
+})
+
+def create_connection(table: SqliteDatabase) -> None:
+	table.connect()
+
+def discard_connection(table: SqliteDatabase) -> None:
+	table.close()
