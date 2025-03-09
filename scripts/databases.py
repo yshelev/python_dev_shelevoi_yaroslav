c8eea0125a6f0609ae1e5d003b18f83d54bc3ee9
zxcpisya <mamin.papa0@gmail.com>
2025-03-09 20:47:27 +1000

    move funcs from databases.py

--- scripts/databases.py
+++ scripts/databases.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASES = '../databases/'
+DATABASES = 'databases/'
 
 logs = os.getenv("logs_database")
 authors = os.getenv("authors_database")
@@ -14,9 +14,3 @@ logs_db = SqliteDatabase(DATABASES + logs, pragmas= {
 authors_db = SqliteDatabase(DATABASES + authors, pragmas= {
 	'foreign_keys': 1
 })
-
-def create_connection(table: SqliteDatabase) -> None:
-	table.connect()
-
-def discard_connection(table: SqliteDatabase) -> None:
-	table.close()
