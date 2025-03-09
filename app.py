ecfb4e56ad9500faa2f0f25bc462c60a8b600556
zxcpisya <mamin.papa0@gmail.com>
2025-03-09 20:47:01 +1000

    completed second request

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 from scripts.models import *
 from flask import Flask, request
 
+from scripts.services import (
+    get_count_of_logins_logouts_and_blog_activities_by_date,
+    create_connection,
+    discard_connection
+)
+
 app = Flask(__name__)
 
-@app.route("/api/comments/<username>", methods=['GET'])
+@app.route("/api/comments", methods=['GET'])
 def comments_of_user():
+    username = request.args.get("username")
 
     return 123
 
-@app.route("/api/general/<username>", methods=['GET'])
+@app.route("/api/general", methods=['GET'])
 def general_info_about_user():
     username = request.args.get("username")
 
+    create_connection(authors_db)
     user = User.get(User.login == username)
+    discard_connection(authors_db)
 
-    request_ = Log.select().where(Log.user_id == user.id)
+    create_connection(logs_db)
+    sql_output = get_count_of_logins_logouts_and_blog_activities_by_date(user.id)
+    discard_connection(logs_db)
 
-    print([i for i in request_])
-    return 'smth'
+    final_output = [{
+        "date": log.date,
+        "login_count": log.login_count,
+        "logout_count": log.logout_count,
+        "blog_activities_count": log.blog_activities_count,
+    } for log in sql_output]
+    return final_output
